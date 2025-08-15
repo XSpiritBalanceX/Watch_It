b4ef6121cc0aef2d4a6ef5b535978114ac42f636
Helen <lenagreidon@mail.ru>
2025-08-15 13:43:02 +0300

    created user controller and jwt strategy

---
+++ server/src/auth/jwt.strategy.ts
@@ -0,0 +1,29 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { UserService } from "src/user/user.service";
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    private configService: ConfigService,
+    private userService: UserService,
+  ) {
+    const secret: string | undefined = configService.get("JWT_SECRET");
+
+    if (!secret) {
+      throw new Error("JWT_SECRET is not defined in the configuration");
+    }
+
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: true,
+      secretOrKey: secret,
+    });
+  }
+
+  async validate({ id }: { id: string }) {
+    return this.userService.getById(id);
+  }
+}
