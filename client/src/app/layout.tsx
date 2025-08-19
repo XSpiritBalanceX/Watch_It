b4b9872fd74f2ac327d849fa3f9ed6b214063dee
Helen <lenagreidon@mail.ru>
2025-08-19 16:38:59 +0300

    initialization next app

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Noto_Sans } from "next/font/google";
+import { SITE_NAME } from "@/constants/seo.constants";
+import "./globals.scss";
+
+const zen = Noto_Sans({
+  subsets: ["cyrillic", "latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+  variable: "--font-zen",
+  style: ["normal"],
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: SITE_NAME,
+    template: `%s | ${SITE_NAME}`,
+  },
+  description: "Best one for checking movies and series",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={zen.className}>{children}</body>
+    </html>
+  );
+}
